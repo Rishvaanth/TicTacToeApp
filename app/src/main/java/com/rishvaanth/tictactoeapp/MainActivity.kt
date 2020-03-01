e28de45f0083a1ba48cea7d4ea53914731399bef
rishvaanth13 <rishvaanth.t@ril.com>
2020-03-01 05:32:51 +0530

    Logic Set

--- app/src/main/java/com/rishvaanth/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/rishvaanth/tictactoeapp/MainActivity.kt
@@ -29,7 +29,57 @@ class MainActivity : AppCompatActivity() {
             R.id.button_9-> cellId = 9
 
         }
-        Toast.makeText(this, "Button $cellId is Clicked", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "Button $cellId is Clicked", Toast.LENGTH_SHORT).show()
+        playgame(cellId, buttonSelected)
 
+    }
+
+    var activeplayer = 1
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+
+
+
+    fun playgame(cellId:Int, buttonSelected:Button){
+        if (activeplayer == 1) {
+            buttonSelected.text = "X"
+            buttonSelected.setBackgroundResource(R.color.paleRed)
+            player1.add(cellId)
+            activeplayer = 2
+        }
+        else {
+            buttonSelected.text = "O"
+            buttonSelected.setBackgroundResource(R.color.paleGreen)
+            player2.add(cellId)
+            activeplayer = 1
+        }
+        buttonSelected.isEnabled = false
+        checkWinner()
+    }
+
+    fun checkWinner(){
+        var winner = -1
+
+        if (player1.contains(1)&&player1.contains(2)&&player1.contains(3)) winner = 1
+        if (player2.contains(1)&&player2.contains(2)&&player2.contains(3)) winner = 2
+
+        if (player1.contains(4)&&player1.contains(5)&&player1.contains(6)) winner = 1
+        if (player2.contains(4)&&player2.contains(5)&&player2.contains(6)) winner = 2
+
+        if (player1.contains(7)&&player1.contains(8)&&player1.contains(9)) winner = 1
+        if (player2.contains(7)&&player2.contains(8)&&player2.contains(9)) winner = 2
+
+        if (player1.contains(1)&&player1.contains(4)&&player1.contains(7)) winner = 1
+        if (player2.contains(1)&&player2.contains(4)&&player2.contains(7)) winner = 2
+
+        if (player1.contains(2)&&player1.contains(5)&&player1.contains(8)) winner = 1
+        if (player2.contains(2)&&player2.contains(5)&&player2.contains(8)) winner = 2
+
+        if (player1.contains(3)&&player1.contains(6)&&player1.contains(9)) winner = 1
+        if (player2.contains(3)&&player2.contains(6)&&player2.contains(9)) winner = 2
+
+        if (winner == 1) Toast.makeText(this, "Player $winner wins!",Toast.LENGTH_SHORT).show()
+        else if (winner ==2) Toast.makeText(this, "Player $winner wins!",Toast.LENGTH_SHORT).show()
+        else if(player1.size + player2.size == 9) Toast.makeText(this, "Its a Draw",Toast.LENGTH_SHORT).show()
     }
 }
